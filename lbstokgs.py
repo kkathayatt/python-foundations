76ef99facc4fd0424cf6c7ef717851bf0196d45b
Khushi <k.kathayatt@gmail.com>
2026-05-02 17:29:45 +0530

    weight converting program

---
+++ lbstokgs.py
@@ -0,0 +1,6 @@
+name = input ("hello!!, what is your name?? ")
+print("hello " +name+ " this program will convert your weight that is in lbs, to kgs")
+print("so,")
+weight = float(input("enter your weight in lbs"))
+in_kgs = weight * 0.45
+print("so, " + name + " your weight in kgs is", in_kgs)
